53758e45f4a6fc037d171c76620954621090737b
guimelo03 <guilhermemelo03@hotmail.com>
2025-01-28 00:36:00 -0300

    adição da pry-rails e phones ao contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,17 +2,22 @@ class Contact < ApplicationRecord
     #Associations
     belongs_to :kind, optional: true
     has_many :phones
-
+    accepts_nested_attributes_for :phones
     # def birthdate_br
     #     I18n.l(self.birthdate) unless Contact.birthdate.blank?
     # end
 
-    def to_br
-        {
-            name: self.name,
-            email: self.email,
-            birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-        }
+    # def to_br
+    #     {
+    #         name: self.name,
+    #         email: self.email,
+    #         birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    #     }
+    # end
+
+    def as_json(options={})
+        h = super(options)
+        h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
     end
     
     # def author
