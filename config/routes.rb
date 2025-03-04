6a1aed2d0371cb24e4948ffc709a97f20533de08
guimelo03 <guilhermemelo03@hotmail.com>
2025-03-04 19:54:55 -0300

    versionando os controllers (v1 e v2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,39 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :auths, only: [:create]
   resources :kinds
 
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationship/kind'
+  scope module: :v1 do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "1" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationship/kind'
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationship/phones'
-    
-    resource :phone, only: [:update, :create, :destroy]
-    resource :phone, only: [:update, :create, :destroy], path: 'relationship/phones'
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationship/phones'
+      
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationship/phones'
 
-    resource :address, only: [:show, :update]
-    resource :address, only: [:show, :update], path: 'relationship/address'
+      resource :address, only: [:show, :update]
+      resource :address, only: [:show, :update], path: 'relationship/address'
+    end
+  end
+
+  scope module: :v2 do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "2" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationship/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationship/phones'
+      
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationship/phones'
+
+      resource :address, only: [:show, :update]
+      resource :address, only: [:show, :update], path: 'relationship/address'
+    end
   end
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
